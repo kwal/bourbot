12c62cf74da6fbfc1ded6e94135c60031a3270a0
Matthew Kowalski <mdkowalski@gmail.com>
2015-03-27 15:48:29 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var ack = require('ac-koa').require('hipchat'),
+  moment = require('moment'),
+  pkg = require('./package.json'),
+  app = ack(pkg);
+
+var addon = app
+  .addon()
+  .hipchat()
+  .allowRoom(true)
+  .scopes('send_notification');
+
+addon.webhook('room_message', /^\/till$/, function*() {
+  var target = moment().hour(16).minute(0).second(0).utc(),
+    now = moment.utc(),
+    diff = target.diff(now);
+
+  yield this.roomClient.sendNotification('You shall imbibe in ' + moment.duration(diff, "milliseconds").humanize());
+});
+
+app.listen();
